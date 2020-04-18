65cb595aa92ba04fb7258709d7709f2467d0ef6b
Raphael Alex Abreu <rabreu@redhat.com>
2020-04-18 12:29:12 -0300

    CORS Config

--- photoz-restful-api/src/main/java/org/keycloak/example/photoz/album/AlbumService.java
+++ photoz-restful-api/src/main/java/org/keycloak/example/photoz/album/AlbumService.java
@@ -11,13 +11,7 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -45,6 +39,18 @@ public class AlbumService {
 
     @Context
     private HttpServletRequest request;
+
+    @OPTIONS
+    @Path("{path : .*}")
+    public Response options() {
+        return Response.ok("")
+            .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+            .header("Access-Control-Allow-Credentials", "true")
+            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+            .header("Access-Control-Max-Age", "1209600")
+            .build();
+    }
 
     @POST
     @Consumes("application/json")
