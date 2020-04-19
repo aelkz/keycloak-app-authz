fd15e177dd610d616185bd48df7bf22ddd53050f
Raphael Alex Abreu <rabreu@redhat.com>
2020-04-19 17:41:26 -0300

    CORS Config

--- photoz-html5-client/src/main/webapp/js/app.js
+++ photoz-html5-client/src/main/webapp/js/app.js
@@ -72,7 +72,7 @@ module.controller('GlobalCtrl', function ($scope, $http, $route, $location, Albu
     }
 });
 
-module.controller('TokenCtrl', function ($scope, Identity) {
+module.controller('TokenCtrl', function ($http, $scope, Identity) {
     $scope.status = function () {
         $http.get(apiUrl + '/health').success(function (data) {
             document.getElementById("output").innerHTML = JSON.stringify(data);
